076521bbb883b2a54f69f8100ea5f6b57a914e2b
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-25 01:13:09 +0800

    update for testing create_db.py

--- models.py
+++ models.py
@@ -13,4 +13,15 @@ class Item(Base):
     def __repr__(self):
         return f"<Item name = {self.name}, price = {self.price}>"
 
+class Item2(Base):
+    __tablename__ = 'items2'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(Text)
+    price = Column(Integer, nullable=False)
+    on_offer = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Item name = {self.name}, price = {self.price}>"
+
 
