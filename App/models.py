ae08610b328e0e907e4028e7388d47bc63894703
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-28 21:49:17 +0800

    test

--- App/models.py
+++ App/models.py
@@ -19,9 +19,14 @@ class Item2(Base):
     name = Column(String(255), nullable=False, unique=True)
     description = Column(Text)
     price = Column(Integer, nullable=False)
-    on_offer = Column(Boolean, default=False)
+    on_offer = Column(Boolean, default=False) #nullable = False means NOT NULL
 
     def __repr__(self):
         return f"<Item name = {self.name}, price = {self.price}>"
+
+class Test(Base):
+    __tablename__ = "test"
+    applier_id = Column(Integer, primary_key=True)
+    accepter_id = Column(Integer)
 
 
