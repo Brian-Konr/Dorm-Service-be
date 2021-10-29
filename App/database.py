f76351cca0b28ce270b428b5984dea944cbb5586
Sherry Yeh <b08705053@ntu.edu.tw>
2021-10-30 02:32:45 +0800

    [Yeh] 加入.env檔案

--- App/database.py
+++ App/database.py
@@ -3,8 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-# engine = create_engine("postgresql+psycopg2://postgres:1515225Ya@localhost:5432/dorm_service", echo = True)
 engine = create_engine("postgresql://postgres:adgj123456@localhost/dorm_service", echo = True)
+
+
+# 葉的
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# POSGRES_URI = os.getenv('POSGRES_URI')
+# engine = create_engine(POSGRES_URI, echo = True)
+
 
 Base = declarative_base()
 
