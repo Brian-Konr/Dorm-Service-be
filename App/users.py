c3577681c13fb032eeb211410fd09a6d93e3d323
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-30 12:27:31 +0800

    sqlacodegen is magic

--- App/users.py
+++ App/users.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+import models
+from database import SessionLocal
+
+db = SessionLocal()
 
 router = APIRouter()
 
 @router.get("/users/", tags=["users"])
 async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    return db.query(models.User).all()
