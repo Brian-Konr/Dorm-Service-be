9a731c80174d71243b11a7ab5324ef6a01ebd172
WU,CHIEN YIN <wcy881218@gmail.com>
2021-11-04 21:22:14 +0800

    Update database.py

--- App/database.py
+++ App/database.py
@@ -3,7 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+<<<<<<< Updated upstream
 engine = create_engine("postgresql://postgres:adgj123456@localhost/dorm_service", echo = True)
+=======
+engine = create_engine("postgresql+psycopg2://postgres:wujenny1218@localhost:5432/dorm_service", echo = True)
+
+>>>>>>> Stashed changes
 
 
 # 葉的
