da8b8e85e696f8ef6a8fbf0cddbfc687cbf8ace0
Shannon1923 <86880073+Shannon1923@users.noreply.github.com>
2021-11-05 17:26:51 +0800

    Update appliers.py

--- App/appliers.py
+++ App/appliers.py
@@ -19,3 +19,25 @@ async def get_all_appliers_for_this_request(request_id: int):
     appliers = db.query(models.Applier).filter(models.Applier.request_id == request_id).all()
     appliers_id = (o.applier_id for o in appliers)
     return db.query(models.User).filter(models.User.user_id.in_(appliers_id)).all()
+
+
+class Apply(BaseModel): #serializer
+    applierId: int
+    requestId: int
+
+    class Config:
+        orm_mode= True
+
+@router.post('/apply/', status_code= status.HTTP_201_CREATED)
+async def make_a_application(item: Apply):
+
+    new_applier = models.Applier(  
+        applier_id = item.applierId,
+        request_id = item.requestId,
+        status = 0
+    )
+
+    db.add(new_applier)
+    db.commit()
+
+    return
